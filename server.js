8bcba04fc692185c85e48b7d9612ea38c1584d52
vinay-mane <manevinay44+github@gmail.com>
2022-11-16 13:39:52 +0530

    try catch

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 const pd = require('pdf-merger-js')
 const pdf = new pd()
 const upload = multer({dest:'uploads/'})
-const app = express()
+const app = express();
+const isPDF = require("is-pdf-valid");
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
@@ -13,21 +14,30 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', upload.array('pdf',15), async(req,res,next)=>{
-  if(req.files.length==0){
-    res.sendFile(path.join(__dirname,"templates/index.html"))
+
+ 
+  try{
+
+  if(!req.files){
+   return res.sendFile(path.join(__dirname,"templates/index.html"))
   }
   for(let i=0 ; i<req.files.length ;i++) {
-    await pdf.add(path.join(__dirname,req.files[i].path))
+      await pdf.add(path.join(__dirname,req.files[i].path))
   }
   await pdf.save('static/'+req.files[0].filename+'.pdf')
   res.download('static/'+req.files[0].filename+'.pdf')
-  res.sendFile(path.join(__dirname,"templates/index.html"))
   setTimeout(()=>{
     for(let i=0 ; i<req.files.length ;i++) {
       fs.unlinkSync(path.join(__dirname,req.files[i].path));
     }
     fs.unlinkSync('static/'+req.files[0].filename+'.pdf');
   },1000*60*2)
+  }
+  catch(err){
+    // console.log(err);
+    return res.sendFile(path.join(__dirname,"templates/index.html"))
+  }
+
 })
 
 
